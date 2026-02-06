2504fb2ce994515f66f3ae43488ad29f97080848
Jiya Choudhary <jiyac3729@gmail.com>
2026-02-06 17:27:13 +0530

    complete

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,59 @@
+/*
+ Question-What are the most optimal skills to learn (aka it's in high demand and a high paying skill)
+ identify skills in high demand and associated with high avg salaries for data analyst roles
+ concentrate on remote position with specified salaries
+ why?  Targets skills that offer job security(high demand) and financial benefits(high salary), offering strategic insights for career development in data analysis
+ */
+
+WITH skills_demand AS(
+  SELECT skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+  FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+  WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+  GROUP BY skills_dim.skill_id
+),
+average_salary AS(
+  SELECT skills_job_dim.skill_id,
+   
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_sal
+  FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+  WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+  GROUP BY skills_job_dim.skill_id
+)
+SELECT skills_demand.skill_id,
+  skills_demand.skills,
+  demand_count,
+  avg_sal
+FROM skills_demand
+  INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+  WHERE demand_count>10
+  ORDER BY avg_sal DESC
+  LIMIT 25;
+
+
+  --rewriting this same query more precisely
+
+  SELECT
+  skills_dim.skill_id,
+  skills_dim.skills,
+  COUNT(skills_job_dim.job_id) AS demand_count,
+  ROUND(AVG(job_postings_fact.salary_year_avg),0) AS avg_sal
+  FROM job_postings_fact
+   INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+  WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+  GROUP BY skills_dim.skill_id
+  HAVING COUNT(skills_job_dim.job_id)>10
+  ORDER BY avg_sal DESC
+  LIMIT 25;
+
